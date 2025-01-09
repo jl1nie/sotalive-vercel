34cca56ad7e9173a671be09aa3c6accad82b7bed
Minoru Tomobe <minoru.tomobe@gmail.com>
2025-01-09 23:58:14 +0900

    Change repose properties to camelCase.

--- public/common/js/revgeocoder.js
+++ public/common/js/revgeocoder.js
@@ -51,7 +51,7 @@ async function local_reverse_geocoder_gsi(lat, lng, elev) {
 	let result = await res2.json()
 
 	if (result.MuniCode) {
-		result['Municipality'] = res['results']['lv01Nm'];
+		result['municipality'] = res['results']['lv01Nm'];
 		result.errors = 'OK';
 		if (elev) {
 			const p_elev = res_elev
